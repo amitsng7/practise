e3d1c611a2afb0f816051061b0cd93455fc0b2ef
amitsng7 <sng.amit7@gmail.com>
2017-10-12 16:29:43 -0400

    started address app and model

---
+++ thanos/addresses/models.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+class Address(models.Model):
+    street = models.CharField(max_length=140)
+    landmark = models.CharField(max_length=140)
+    city = models.CharField(max_length=140)
+    zipcode = models.ForeignKey(Zipcode)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering=["-timestamp"]
+
+    # @property
+    # def comments(self):
+    #     instance = self
+    #     qs = Comment.objects.filter_by_instance(instance)
+    #     return qs
+
+    # @property
+    # def get_content_type(self):
+    #     instance = self
+    #     content_type = ContentType.objects.get_for_model(instance.__class__)
+    #     return content_type
