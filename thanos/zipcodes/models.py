9c465a2835f10356ad95da4d2ba59cbed4803706
amitsng7 <sng.amit7@gmail.com>
2017-10-12 16:46:30 -0400

    zipcode model

--- thanos/zipcodes/models.py
+++ thanos/zipcodes/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+class Zipcode(models.Model):
+    zipcode = models.IntegerField(primary_key=True, max_length=10)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=30)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering=["-timestamp"]
